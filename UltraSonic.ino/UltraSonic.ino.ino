07dabd6a1ffe85856ee9c6124cf35332aff29464
CamosunRobotics <56321168+CamosunRobotics@users.noreply.github.com>
2019-10-08 13:52:25 -0700

    Add files via upload
    Jer's ultrasonic code

---
+++ UltraSonic.ino/UltraSonic.ino.ino
@@ -0,0 +1,48 @@
+
+#define uSonic1 10
+#define uSonic2 9
+#define servo1pin 8
+#define servo2pin 7
+#include <Servo.h>
+
+Servo sonicServo1;
+
+
+long sonicTime(char uSonic);
+float sonicMeters(char uSonic);
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  pinMode(uSonic1, OUTPUT);
+  pinMode(uSonic2, OUTPUT);
+
+
+}
+
+void loop()
+{
+
+  Serial.println(sonicMeters(uSonic2));
+  delay(200);
+
+}
+
+long sonicTime(char uSonic)
+{ // The same pin is used to read the signal
+
+  pinMode(uSonic, OUTPUT);
+  digitalWrite(uSonic, LOW);
+  delayMicroseconds(2);
+  digitalWrite(uSonic, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(uSonic, LOW);
+  pinMode(uSonic, INPUT);
+
+  return (pulseIn(uSonic, HIGH));
+}
+float sonicMeters(char uSonic)
+{
+  return(sonicTime(uSonic) * 3.43 / 1000.0 / 2.0);
+}
